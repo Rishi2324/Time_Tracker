a3485681b8a2338301ca02b087601c830dc6775e
Rishi2324 <s.rishi2324@gmail.com>
2026-02-26 01:22:23 +0530

    Updated

--- vite.config.js
+++ vite.config.js
@@ -1,26 +1,33 @@
-VitePWA({
-  registerType: 'autoUpdate',
-  includeAssets: ['icon-192.png', 'icon-512.png'],
-  manifest: {
-    name: 'Time Tracker',
-    short_name: 'Tracker',
-    description: 'Time tracking app',
-    theme_color: '#000000',
-    background_color: '#ffffff',
-    display: 'standalone',
-    start_url: '/',
-    scope: '/',
-    icons: [
-      {
-        src: '/icon-192.png',
-        sizes: '192x192',
-        type: 'image/png'
-      },
-      {
-        src: '/icon-512.png',
-        sizes: '512x512',
-        type: 'image/png'
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
+
+export default defineConfig({
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Time Tracker',
+        short_name: 'Tracker',
+        description: 'Time tracking app',
+        theme_color: '#000000',
+        background_color: '#ffffff',
+        display: 'standalone',
+        start_url: '/',
+        icons: [
+          {
+            src: '/icon-192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '/icon-512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
       }
-    ]
-  }
+    })
+  ]
 })
